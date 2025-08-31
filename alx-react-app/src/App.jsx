c1aeca9a99c9558882867299c26807499e851c43
Berto1957 <banjofoluso@gmail.com>
2025-08-31 12:29:23 +0100

    Add React components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+// App.jsx
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <WelcomeMessage />
-      <button onClick={() => setCount(count + 1)}>
-        Count is {count}
-      </button>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
