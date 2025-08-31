1bffaa779c023389cfbe2780b88a6862419cdf18
Berto1957 <banjofoluso@gmail.com>
2025-08-31 13:57:58 +0100

    Added WelcomeMessage component and imported it in App.js

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Header />
-      <WelcomeMessage name="Momo" />
+      <WelcomeMessage name="Alice" />
       <MainContent />
       <Footer />
     </div>
