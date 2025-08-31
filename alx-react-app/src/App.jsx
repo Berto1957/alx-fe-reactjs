b50d327108d3a6fa2a05263fffdaf017d93cf22d
Berto1957 <banjofoluso@gmail.com>
2025-08-31 15:19:02 +0100

    Render UserProfile and WelcomeMessage components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,16 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
+      <WelcomeMessage name="Momo" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
