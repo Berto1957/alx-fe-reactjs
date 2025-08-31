585e3584f094a7d6dd7ca7a5175bafcc09d1588d
Berto1957 <banjofoluso@gmail.com>
2025-08-31 14:48:09 +0100

    Render all components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,18 @@
-import WelcomeMessage from './components/WelcomeMessage';
 import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-
