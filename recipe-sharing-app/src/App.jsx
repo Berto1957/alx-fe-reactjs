dc5a63bc6c5cdf623b21f244a32401f60366fc67
Berto1957 <banjofoluso@gmail.com>
2025-09-17 01:31:39 +0100

    Implement routing with react-router-dom in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import RecipeDetail from './components/RecipeDetail';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import RecipeList from './components/RecipeList';
-import TimeFilter from './components/TimeFilter';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import SearchBar from "./components/SearchBar";
+import RecipeList from "./components/RecipeList";
+import RecipeDetail from "./components/RecipeDetail";
+import EditRecipeForm from "./components/EditRecipeForm";
+
+const App = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>🍲 Recipe Sharing App</h1>
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-      <SearchBar />
-      <TimeFilter />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
