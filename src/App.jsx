63b3e4f27705292f4e9c97a50c91b541019a3b4b
Berto1957 <banjofoluso@gmail.com>
2025-09-21 18:37:10 +0100

    Implement advanced search, filtering, and routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
